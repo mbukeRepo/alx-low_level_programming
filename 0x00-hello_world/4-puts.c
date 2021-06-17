efc15e0ba13ebee3d1586bcd7430d6ada8882a0f
mbukeRepo <mbuke404@gmail.com>
2021-06-17 10:26:33 -0700

    this is the end

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-main(void)/*this is the usage of the puts function*/
+int main(void)/*this is the usage of the puts function*/
 {
 char str[] = "\"Programming is like building a multilingual puzzle";
 puts(str);
+return (0);
 }
