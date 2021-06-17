07f22891648c3aa3d1e539064c2ecd8f794ce11c
mbukeRepo <mbuke404@gmail.com>
2021-06-17 11:32:02 -0700

    this is the end

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - prints stuff
- * Return - 0 for successful return 
+ * Return: - 0 for successful return
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);  
+return (0);
 }
