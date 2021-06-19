8012a48ab67e642843ee5bfc218f61bdb2deb5bb
mbukeRepo <mbuke404@gmail.com>
2021-06-19 08:01:01 -0700

    this is the end

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is real
+ * Return: 0 for successful exit 
+ */
+int main(void)
+{
+char str[5] = "this";
+int i =0;
+while (i<5)
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
