f4ec900ee88956f72ee25e865394e3aa9e141479
mbukeRepo <mbuke404@gmail.com>
2021-06-21 12:28:42 -0700

    this is the end

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
 
-/**
- * main - this is good
- * Return: 0 for the successful
- */
-int main(void)
-{
-char str[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int i = 0;
-while (i < 52)
-{
-putchar(str[i]);
-i++;
-}
-putchar('\n');
-return (0);
-}
