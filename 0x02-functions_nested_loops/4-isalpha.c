9810ba48e56cf6a1a307a52a810e385d789c0553
mbukeRepo <mbuke404@gmail.com>
2021-06-22 12:50:54 -0700

    this is the end

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+  * _isalpha - fuck
+  * @c: any char
+  * Return: return 1 for success
+  */
+int _isalpha(int c)
+{
+if (isalpha(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
