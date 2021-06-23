6eb9d5a286054bcd8245057c918f27c4267c8b03
mbukeRepo <mbuke404@gmail.com>
2021-06-23 12:03:45 -0700

    this is the end

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - does stuff
+ * @n: number
+ * Return: number
+ */
+int print_last_digit(int n)
+{
+int lastDigit = abs(n) % 10;
+_putchar(lastDigit + '0');
+return (lastDigit);
+}
