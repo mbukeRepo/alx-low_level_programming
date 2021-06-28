38e61742de333dd066bcfc82215f4e3aa461e6a0
mbukeRepo <mbuke404@gmail.com>
2021-06-28 12:40:39 -0700

    this is the end

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,10 +3,10 @@
  * _strcpy - copies strings
  * @dest: destination
  * @src: source
- * Return : pointer to a string
+ * Return: pointer to a string
  */
 char *_strcpy(char *dest, char *src)
 {
-strcpy(dest,src);
-return dest;
+strcpy(dest, src);
+return (dest);
 }
