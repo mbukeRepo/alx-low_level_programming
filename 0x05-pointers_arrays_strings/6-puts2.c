8886e20aae0c04f955931c4da8da31904442a964
mbukeRepo <mbuke404@gmail.com>
2021-06-28 12:17:24 -0700

    this is the end

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * puts2 - prints even numbers
+ * @str: string to print
+ */
+
+void puts2(char *str)
+{
+size_t i = 0;
+while (i < strlen(str))
+{
+if (str[i] % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+_putchar('\n');
+}
