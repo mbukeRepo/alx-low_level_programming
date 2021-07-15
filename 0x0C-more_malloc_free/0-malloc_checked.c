0293a591f8843fc38b123bf844c0bb64a5db01ad
mbukeRepo <mbuke404@gmail.com>
2021-07-15 11:08:42 -0700

    this is the end

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+
+#include <stdlib.h>
+/**
+ * malloc_checked - does stuff
+ * @b: size of memory allocated
+ * Return: pointer to void
+ */
+void *malloc_checked(unsigned int b)
+{
+void *s;
+s = malloc(b);
+if (!s)
+{
+exit(98);
+}
+return (s);
+}
