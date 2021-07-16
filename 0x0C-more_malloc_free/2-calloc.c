d986b7954182cdc2d01f6b396d42778deb9a6b86
mbukeRepo <mbuke404@gmail.com>
2021-07-16 04:40:07 -0700

    this is the end

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _calloc - implements calloc
+ * @nmemb: members of the array
+ * @size: size of each member
+ * Return: void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *newArray;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+newArray = malloc(nmemb * size);
+if (newArray == NULL)
+{
+return (NULL);
+}
+newArray = memset(newArray, 0, nmemb);
+return (newArray);
+}
