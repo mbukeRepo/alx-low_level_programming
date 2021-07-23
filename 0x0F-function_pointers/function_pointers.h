03f83d0ad243e40c09aa8d3a7c155ba648796180
mbukeRepo <mbuke404@gmail.com>
2021-07-23 00:23:38 -0700

    this is the end

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,9 +2,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/**
+/*
  * File: function_pointers.h
- * AUth: Mbuke Prince
+ * AUth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x0E-function_pointers directory.
  */
