7db1518db77e0f5ff8cd0894273098255a59b300
mbukeRepo <mbuke404@gmail.com>
2021-08-10 08:09:26 +0200

    making changes

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
