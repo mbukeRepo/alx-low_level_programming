3d8c11beb55aff39bc7ffce1ec31d5be72c8deae
mbukeRepo <mbuke404@gmail.com>
2021-09-02 18:43:37 +0200

    corrected the string_nconcat function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,5 @@
 #include <stdlib.h>
 #include <string.h>
-/**
- * sup - concatenates strings
- * @s1: first string
- * @s2: second string
- * @n: number of characters
- * @newlen: length of new string
- * @dest_len: lenght of s1
- * @newstr: new string
- * Return: string
- */
-char *sup(char *s1, char *s2, char *newstr, int dest_len, int newlen, int n)
-{
-int i = 0;
-while (i < newlen)
-{
-
-if (i < dest_len)
-{
-newstr[i] = s1[i];
-}
-if (i >= dest_len && i < newlen)
-{
-if (n == 0)
-{
-i++;
-break;
-}
-newstr[i] = s2[i - dest_len];
-}
-i++;
-}
-newstr[i] = '\0';
-return (newstr);
-}
-
 
 /**
  * string_nconcat - concatenates strings
@@ -45,41 +10,30 @@ return (newstr);
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int dest_len;
-int newlen;
-char *newstr;
-
+char *concat;
+unsigned int len = n, index;
 if (s1 == NULL)
 {
-dest_len = 0;
+s1 = "";
 }
-
 if (s2 == NULL)
 {
-n = 0;
+s2 = "";
 }
+for (index = 0; s1[index]; index++)
+len++;
 
-if (s1 != NULL)
-{
-dest_len = strlen(s1);
-}
-
-if (s2 != NULL && n >= strlen(s2))
-{
-n = strlen(s2);
-}
-
-
-newlen = dest_len + n;
-if (dest_len == 0 && n == 0)
-{
-newlen = 1;
-}
-newstr = malloc(newlen *sizeof(char));
-if (newstr == NULL)
-{
-free(newstr);
+concat = malloc(sizeof(char) * (len + 1));
+if (concat == NULL)
 return (NULL);
-}
-return (sup(s1, s2, newstr, dest_len, newlen, n));
+
+len = 0;
+for (index = 0; s1[index]; index++)
+concat[len++] = s1[index];
+
+for (index = 0; s2[index] && index < n; index++)
+concat[len++] = s2[index];
+
+concat[len] = '\0';
+return (concat);
 }
