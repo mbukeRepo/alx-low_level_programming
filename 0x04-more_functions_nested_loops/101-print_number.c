f98e03e798bd9d4a306e8079beb872056c252ae7
mbukeRepo <mbuke404@gmail.com>
2021-09-02 19:51:54 +0200

    added a function to print nums

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,14 +5,15 @@
  */
 void print_number(int n)
 {
+int num = n;
 if (n < 0)
 {
 _putchar('-');
-n = n * -1;
+num = -num;
 }
-if (n / 10)
+if ((num / 10) > 0)
 {
-print_number(n / 10);
+print_number(num / 10);
 }
-_putchar(n % 10 + '0');
+_putchar(num % 10 + '0');
 }
