6c93d15477bbf850782e4ead2a7f0ca1a6ef43d0
mbukeRepo <mbuke404@gmail.com>
2021-09-16 17:53:52 +0200

    null checking

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,6 +11,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 dlistint_t *new, *tmp;
 tmp = *head;
 new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
 new->n = n;
 new->next = NULL;
 
