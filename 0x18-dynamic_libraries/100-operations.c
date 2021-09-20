65d6fb43339fbaf64b9b385592f536583cbabe4e
mbukeRepo <mbuke404@gmail.com>
2021-09-20 17:30:12 +0200

    added python compatibility of c funcs

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+/**
+ * mod - finds modulus for two numbers
+ * @a: first number
+ * @b: second number
+ * Return: number
+ */
+int mod(int a, int b)
+{
+return (a % b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: seconde number
+ * Return: number
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: seconde number
+ * Return: number
+ */
+int div(int a, int b)
+{
+return (a / b);
+}
+
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: seconde number
+ * Return: number
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first number
+ * @b: seconde number
+ * Return: number
+ */
+int sub(int a, int b)
+{
+return (a - b);
+}
