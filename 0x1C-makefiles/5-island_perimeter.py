7a0e73e69cd1642409ef5e75fafec7456727058d
mbukeRepo <mbuke404@gmail.com>
2021-11-24 15:48:51 +0200

    solved the mandatory tasks of the makefile assignment

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Defines a functions that finds the perimeter of island """
+
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island land
+    Args:
+       grid (list): 2-dimension matrix containing 1s and 0s
+    Returns:
+       perimeter of the island
+    """
+    h = len(grid)
+    w = len(grid[0])
+
+    squares = 0
+    unwanted_sides = 0
+
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == 1:
+                squares += 1
+                if grid[i][j - 1] == 1:
+                    unwanted_sides += 1
+
+                if grid[i - 1][j] == 1:
+                    unwanted_sides += 1
+
+    return squares * 4 - unwanted_sides * 2
